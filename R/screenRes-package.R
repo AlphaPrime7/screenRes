e65bc02f0743fb2bec8c7bf4159ea62c55c23288
alphaprime7 <awesome.tingwei@outlook.com>
2024-10-03 17:25:32 -0500

    Initial commit

---
+++ R/screenRes-package.R
@@ -0,0 +1,12 @@
+## screenRes - R package to get the screen resolution
+## Copyright (C) 2024 Tingwei Adeck
+
+#' @keywords internal
+#' @aliases screenRes-package
+"_PACKAGE"
+utils::globalVariables(
+  names = c("%>%",
+            "numscreens"
+            )
+)
+NULL
